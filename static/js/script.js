2e36716aeb77ddb2cf6942cfdd872028ac3ac7bd
Caroline Zwolinski <carrozw@gmail.com>
2021-02-23 09:52:47 +0100

    add register form, materialize

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+    //Navigation
     $('.sidenav').sidenav();
-  });
+
+    //Confirm password
+    $("#password").on("focusout", function (e) {
+        if ($(this).val() != $("#passwordConfirm").val()) {
+            $("#passwordConfirm").removeClass("valid").addClass("invalid");
+        } else {
+            $("#passwordConfirm").removeClass("invalid").addClass("valid");
+        }
+    });
+    $("#passwordConfirm").on("keyup", function (e) {
+        if ($("#password").val() != $(this).val()) {
+            $(this).removeClass("valid").addClass("invalid");
+        } else {
+            $(this).removeClass("invalid").addClass("valid");
+        }
+    });
+});
